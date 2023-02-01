38501ea59aa23333c06b4f2dd423aae563f3ae12
WGT <williamgeorgethomas@gmail.com>
2023-01-31 21:54:59 -0600

    modified handlebars views, updated server. now have functioning login, logout authentication

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const postRoutes = require('./post-routes.js');
+const postRoutes = require("./post-routes.js");
+const userRoutes = require("./user-routes");
 
-router.use('/post', postRoutes);
+router.use("/post", postRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
