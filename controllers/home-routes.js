0bf832d7341b1f3973c8d509c8d99958c317bc4a
WGT <williamgeorgethomas@gmail.com>
2023-02-17 15:06:35 -0600

    started dashboard handlebar view, added delete post route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
     res.json(err);
   });
   const posts = postData.map((post) => post.get({ plain: true }));
-  res.render("all", { posts, loggedIn: req.session.loggedIn });
+  res.render("homepage", { posts, loggedIn: req.session.loggedIn });
 });
 
 router.get("/login", (req, res) => {
@@ -29,5 +29,35 @@ router.get("/dashboard", (req, res) => {
 
   res.redirect("/");
 });
+
+
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
 
 module.exports = router;
