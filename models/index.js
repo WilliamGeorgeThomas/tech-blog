440e4418dbaf069d72f3604a7998fc3d8d8e641b
WGT <williamgeorgethomas@gmail.com>
2023-02-18 17:53:27 -0600

    added comment model and associations. updated styling

--- models/index.js
+++ models/index.js
@@ -2,8 +2,17 @@ const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
 
-
 Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
   foreignKey: "userId",
   onDelete: "CASCADE",
 });
