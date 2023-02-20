41c296cbd7e6223347a4fe3c67d27f214969291c
WGT <williamgeorgethomas@gmail.com>
2023-02-20 14:04:52 -0600

    comment box on posts now functional

--- public/js/post.js
+++ public/js/post.js
@@ -4,8 +4,10 @@ const newFormHandler = async (event) => {
   const body = document.querySelector("#comment-body").value.trim();
 
   if (body) {
-    const id = event.target.getAttribute("user_id");
-    const response = await fetch(`/post/${id}`, {
+    const id = event.target.getAttribute("data-id");
+    console.log(id);
+    // const id = 5;
+    const response = await fetch(`/api/post/${id}`, {
       method: "POST",
       body: JSON.stringify({ body }),
       headers: {
@@ -15,28 +17,29 @@ const newFormHandler = async (event) => {
 
     if (response.ok) {
       alert("comment added!");
+      document.location.reload();
     } else {
-      alert("Failed to create post");
+      alert("Failed to add comment");
     }
   }
 };
 
-const delButtonHandler = async (event) => {
-//   event.preventDefault();
+// const delButtonHandler = async (event) => {
+// //   event.preventDefault();
 
-  if (event.target.hasAttribute("data-id")) {
-    const id = event.target.getAttribute("data-id");
+//   if (event.target.hasAttribute("data-id")) {
+//     const id = event.target.getAttribute("data-id");
 
-    const response = await fetch(`/api/post/${id}`, {
-      method: "DELETE",
-    });
+//     const response = await fetch(`/api/post/${id}`, {
+//       method: "DELETE",
+//     });
 
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert("Failed to delete post");
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace("/dashboard");
+//     } else {
+//       alert("Failed to delete post");
+//     }
+//   }
+// };
 
-document.querySelector(".new-comment-form").addEventListener("submit", newFormHandler);
+document.querySelector(".new-comment-form").addEventListener("click", newFormHandler);
