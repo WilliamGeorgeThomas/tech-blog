7f1edce1a749c303ce08138cd85b7bbb800a2b66
WGT <williamgeorgethomas@gmail.com>
2023-02-20 20:15:28 -0600

    updated dashboard js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -45,6 +45,7 @@ function revealForm() {
   document.querySelector(".new-post-form").classList.remove("hidden");
 }
 
+
+document.querySelector(".reveal-btn").addEventListener("click", revealForm);
 document.querySelector(".new-post-form").addEventListener("submit", newFormHandler);
 document.querySelector(".post-list").addEventListener("click", delButtonHandler);
-document.querySelector(".reveal-btn").addEventListener("click", revealForm);
